9f5492764110e595f04070f3eba259724ab8b47c
daesung <eogud8203@naver.com>
2024-08-06 16:20:24 +0900

    [Feat] Model 구현 완료

---
+++ PokemonBook/Model/PokeInfo.swift
@@ -0,0 +1,8 @@
+//
+//  PokeInfo.swift
+//  PokemonBook
+//
+//  Created by 머성이 on 8/4/24.
+//
+
+import Foundation
---
+++ PokemonBook/ViewModel/MainViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MainViewModel.swift
+//  PokemonBook
+//
+//  Created by 머성이 on 8/5/24.
+//
+
+import Foundation
